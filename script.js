d53e3951f49960df1ccad6bb2a887b07f1e23f4e
thisisutkarsh <kr.utkarsh777@gmail.com>
2020-04-28 20:01:00 +0530

    Added responsiveness for mouse-click
    Added sound emission on touch and on mouse click.

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-function playSound(e) {
-    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`)
-    const key = document.querySelector(`div[data-key = "${e.keyCode}"]`)
-    if (!audio)
-        return; // stop when other keys are pressed
+window.addEventListener("keydown", (e) => {
+    playSound(e.keyCode);
+});
+// Playsound for keypress
+function playSound(key) {
+    const audio = document.querySelector(`audio[data-key = "${key}"]`);
+    if (!audio) return; // stop when other keys are pressed
     audio.currentTime = 0; // rewind to the start
     audio.play();
-    key.classList.add("playing");
+    const div = document.querySelector(`div[data-key = "${key}"]`);
+    div.classList.add("playing");
 }
 
+// Removing transition
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+// First wait for ending of transition and then call removeTransitition
+//Remove transition
 function removeTransition(e) {
-    if (e.propertyName !== "transform")
-        return;
+    if (e.propertyName !== "transform") return;
     this.classList.remove("playing");
 }
-const keys = document.querySelectorAll(".key");
-keys.forEach(key => key.addEventListener("transitionend", removeTransition)); // first add transition and then call removeTransitition
-window.addEventListener("keydown", playSound);
+
+
+// Making responsive to mouse click
+document.querySelectorAll("div.key").forEach((div) => {
+    div.addEventListener("click", handleClick);
+});
+
+function handleClick() {
+    const key = this.dataset.key;
+    // To access any data- element in JS, we use "dataset" property. If we have data - key, it becomes: dataset.key
+    if (key) {
+        playSound(key);
+    }
+}
